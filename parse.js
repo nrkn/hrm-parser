08305c2b5300347706ab6c57b6b00ff24f2c819a
Nik Coughlin <nrkn.com@gmail.com>
2015-10-27 15:30:23 +1300

    Initial commit

---
+++ parse.js
@@ -0,0 +1,100 @@
+require( './polyfills' )
+
+const oneArity = [ 'INBOX', 'OUTBOX' ]
+const twoArity = [ 'COPYFROM', 'COPYTO', 'ADD', 'SUB', 'BUMPUP', 'BUMPDN', 'JUMP', 'JUMPZ', 'JUMPN', 'COMMENT' ]
+const jumps = [ 'JUMP', 'JUMPZ', 'JUMPN' ]
+
+const regex = {
+  comment: /\-\-\s.*?\s\-\-/g,
+  define: /DEFINE\s+(?:COMMENT|LABEL)\s+\d+\s+eJ[-a-z0-9+/=\s]+;/gi,
+  label: /^[a-z]:$/i,
+  direct: /^\d+$/,
+  reference: /^\[\d+\]$/,
+  address: /^\d+$|^\[\d+\]$/
+}
+
+const strip = [ regex.comment, regex.define ]
+    
+const     
+    
+const parse = source => {
+  const tokens = strip.reduce( 
+      ( source, regex ) =>
+        source.replace( regex, '' ),
+      source 
+    )
+    .split( /\s+/ )
+    .filter( token => token !== '' )
+    
+  const len = tokens.length
+  var lines = []
+  var i = 0
+  
+  while( i < len ){
+    var current = tokens[ i ]
+    
+    if( regex.label.test( current ) || oneArity.includes( current.toUpperCase() ) ){
+      lines.push( [ current ] )
+      i++
+    } else if( twoArity.includes( current.toUpperCase() ) ){
+      lines.push( [ current, tokens[ i + 1 ] ] )
+      i += 2
+    } else {
+      throw Error( `Unexpected token: "${ current }"` )
+    }
+  }
+  
+  lines = lines.filter( l => l[ 0 ] !== 'COMMENT' )
+  
+  const labels = {}
+  i = 0
+  
+  lines = lines.reduce( ( lines, words ) => {
+    const first = words[ 0 ]
+    
+    if( regex.label.test( first ) ){
+      const label = first.replace( ':', '' )
+      
+      labels[ label ] = i
+      
+      return lines
+    }
+    
+    lines.push( words )
+    i++
+    
+    return lines
+  }, [] )  
+  
+  lines = lines.map( words => {
+    const instr = words[ 0 ].toUpperCase()
+    
+    if( twoArity.includes( instr ) ){
+      var arg = words[ 1 ]
+      
+      if( jumps.includes( instr ) ){
+        if( !arg in labels ){
+          throw Error( 'No such label: ' + arg )
+        }
+        
+        arg = labels[ arg ]
+      }
+      
+      if( !regex.address.test( arg ) ){
+        throw Error( 'Invalid address: ' + arg )
+      }
+      
+      if( regex.direct.test( arg ) ){
+        arg = Number( arg )
+      } 
+      
+      return [ instr, arg ]
+    }
+    
+    return [ instr ]
+  })  
+  
+  return lines
+}
+
+module.exports = parse
---
+++ readme.md
@@ -0,0 +1,55 @@
+# hrm-parser
+
+## A parser for Human Resouce Machine programs
+
+![Screenshot](http://tomorrowcorporation.com/blog/wp-content/themes/tcTheme2/images/hrm/screenshots/hrm_04.png)
+
+> Human Resource Machine is a puzzle game. In each level, your boss gives you a job. Automate it by programming your little office worker! If you succeed, you'll be promoted up to the next level for another year of work in the vast office building. Congratulations!
+
+Note that it throws away source comments, comments and labels - if you need 
+these I recommend using [hrm-grammar] instead, it is a proper expression grammar 
+and will produce an AST
+
+### NPM
+
+`npm install hrm-parser`
+
+### Usage
+
+Requires node 4.x - uses ES6
+
+```javascript
+
+const parser = require( 'hrm-parser' )
+
+//source is a string in the Human Resource Machine format
+const source = getSource()
+
+const parsed = parser( source )
+```
+
+It will output something like:
+```json
+[ 
+  [ 'BUMPUP', 24 ],
+  [ 'INBOX' ],
+  [ 'JUMPZ', 9 ],
+  [ 'COPYTO', '[24]' ],
+  [ 'JUMP', 0 ],
+  [ 'COPYFROM', '[19]' ],
+  [ 'OUTBOX' ],
+  [ 'COPYFROM', '[24]' ],
+  [ 'COPYTO', '[19]' ],
+  [ 'BUMPDN', 24 ],
+  [ 'JUMPZ', 0 ],
+  [ 'COPYTO', 23 ],
+  [ 'COPYFROM', 23 ],
+  [ 'COPYTO', 19 ],
+  [ 'BUMPDN', 23 ],
+  [ 'JUMPZ', 5 ],
+  [ 'COPYFROM', '[23]' ],
+  [ 'SUB', '[19]' ],
+  [ 'JUMPN', 12 ],
+  [ 'JUMP', 14 ] 
+]
+```
